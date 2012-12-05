656bb7af1a8eaa6a788e49b9ff4001b042953673
Thomas Eriksson <thomas.eriksson@gmail.com>
2012-12-05 21:57:19 +0100

    added license and some options

--- monospaci.py
+++ monospaci.py
@@ -1,27 +1,65 @@
 #!/usr/bin/python
 #
+# monospaci.py is copyright (C) 2012 
+# by Thomas Eriksson.
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#   The name of the author may not be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+#   EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#   OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+###############################################################################
+
 import fontforge
 import psMat
 import subprocess
 import sys
-
-baseFont = fontforge.open(sys.argv[1])
+import os.path
 namesList = "NamesList.txt"
-mergeFonts = set()
-
+fontList = list()
+glifs = set()
 index = 0
-argv = sys.argv
-for arg in argv:
-    if index != 0 or index != 1:
-        if len(sys.argv[index]) > 4:
-            if argv[index][-4:] == '.ttf' or argv[index][-4:] == '.TTF' or argv[index][-4:] == '.otf' or argv[index][-4:] == '.OTF' or argv[index][-4:] == '.otf' or argv[index][-4:] == '.SFD' or argv[index][-4:] == '.sfd':
-                mergeFonts.add(argv[index])
-            if argv[index][-5:] == '.glif' or argv[index][-5:] == '.GLIF':
-                print argv[index]
-    index = index + 1
 
-fontName = baseFont.fullname + " Mono"
-mergedFont = baseFont
+nextArgIsNamesList = False
+for arg in sys.argv:
+    index = index + 1
+    if nextArgIsNamesList :
+        namesList = arg
+        nextArgIsNamesList = False
+        continue
+    if arg == '-nameslist':
+        nextArgIsNamesList = True
+        continue
+
+for arg in sys.argv:
+    if len(arg) > 4:
+        if arg[-4:] == '.ttf' or arg[-4:] == '.TTF' or arg[-4:] == '.otf' or arg[-4:] == '.OTF' or arg[-4:] == '.otf' or arg[-4:] == '.SFD' or arg[-4:] == '.sfd':
+            fontList.append(arg)
+        if arg[-5:] == '.glif' or arg[-5:] == '.GLIF':
+            glifs.add(arg)
+    index = index + 1
+mergedFont = fontforge.open(fontList[0])
+supplement = " Mono"
+fontName = mergedFont.fontname + supplement
 
 ## use the following ASCII chars as baseWidthChars
 ## the baseWidth will determine the max width
@@ -29,8 +67,6 @@ baseWidthChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n
 
 ## scale all capital letters by at least the same factor as the following letters
 capitalWidthChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
-
-#############################################################
 
 unicodeNamesList = open(namesList)
 lines = unicodeNamesList.readlines()
@@ -41,10 +77,28 @@ for line in lines:
     if line[0] == '@':
         continue
     try:
-        hexval = int(line.split("\t")[0], 16)
-        unicodeNames[line.split("\t")[1]] = hexval
+        hexval = int(line.split()[0], 16)
+        name = line[len(line.split("\t")[0]):-1].lstrip().rstrip()
+        
+        unicodeNames[name] = hexval
     except:
         continue
+
+## preserve aspect correction for some chars
+preserveAspectChars = set() 
+
+for name in unicodeNames.keys():
+    if name.find("CIRCLED") > 0:
+        preserveAspectChars.add(unicodeNames[name])
+
+    if name == "REGISTERED SIGN":
+        preserveAspectChars.add(unicodeNames[name])
+
+    if name == "COPYRIGHT SIGN":
+        print name
+        preserveAspectChars.add(unicodeNames[name])
+
+#############################################################
 
 maxWidth = -1.0
 avgSpacing = 0.0
@@ -77,19 +131,11 @@ for name in unicodeNames:
     if name.find(findName) == 0:
         capitalLetters.add(unicodeNames[name])
 
-maxHeight = -1.0
-for char in baseWidthChars:
-    bounds = mergedFont[char].boundingBox()
-    maxHeight = max(maxHeight, bounds[3] - bounds[1])
-
-## allow 1.5 linespacing
-maxHeight = maxHeight * 1.5 - avgSpacing*2
-
-baseFontUnicodePoints = set()
-for glyphName in baseFont:
-    unicodePoint = baseFont[glyphName].unicode
+mergedFontUnicodePoints = set()
+for glyphName in mergedFont:
+    unicodePoint = mergedFont[glyphName].unicode
     if unicodePoint > 0:
-        baseFontUnicodePoints.add(unicodePoint)
+        mergedFontUnicodePoints.add(unicodePoint)
 
 for anchorClass in ["mark", "base", "ligature", "basemark", "entry", "exit"]:
     try:
@@ -126,6 +172,12 @@ for glyphName in mergedFont:
     bearing = (mergedFont[glyphName].left_side_bearing + mergedFont[glyphName].right_side_bearing)/2.0
 
 ## override some broken glyphs
+for glif in glifs :
+    pos = os.path.basename(glif).find('_')
+    if pos > 0:
+        mergedFont[os.path.basename(glif)[0:pos]].clear()
+        mergedFont[os.path.basename(glif)[0:pos]].importOutlines(glif)
+
 #mergedFont['i'].clear()
 #mergedFont['i'].importOutlines("orig/i_TeXLove.glif")
 #mergedFont['i'].width = maxWidth + avgSpacing*2
@@ -158,10 +210,13 @@ except:
     pass
 
 ## set name and values
-mergedFont.fontname = fontName 
-mergedFont.fullname = fontName 
-mergedFont.familyname = fontName
-#mergedFont.os2_vendor = vendorName
+mergedFont.fontname = fontName.replace(" ","")
+mergedFont.fullname = fontName.replace(" ","")
+mergedFont.familyname = fontName.replace(" ","")
+
+if mergedFont.fontlog == None:
+   mergedFont.fontlog = ""
+mergedFont.fontlog = mergedFont.fontlog + "Modified into monospace by monospaci.py (https://github.com/arnognulf/monospaci.py)\n"
 mergedFont.os2_panose = (2, 9, 5, 9, 0, 0, 0, 0, 0, 0) 
 mergedFont.os2_fstype = 8
 
@@ -169,7 +224,7 @@ fontForgeOutput = fontName + "-Output.ttf"
 ttfAutoHintOutput = "TTFAutoHint-Output.ttf"
 finalOutput = fontName.replace(" ","-")+ "-Regular.ttf"
 
-mergedFont.generate(fontForgeOutput)
+mergedFont.generate(fontForgeOutput, flags=('PfEd-comments',))
 
 ## ttfautohint mangles our OS/2 table, containing vital Monospace bits, stash away...
 subprocess.call(["ttftable", "-export", "OS/2=os2.fontforge", fontForgeOutput])
